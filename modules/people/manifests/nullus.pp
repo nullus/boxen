e0169e98a07d49d8929df90b0e5a5f207c16f2b3
null <null@disasterarea.net>
2017-09-12 11:22:34 +0100

    Fixup minor issues
    Add cache files (as recommended)
    Update dependencies for fish configuration, avoid cycles

--- modules/people/manifests/nullus.pp
+++ modules/people/manifests/nullus.pp
@@ -4,8 +4,21 @@ class people::nullus {
   include sublime_text
   include iterm2::dev
 
+  package { 'fish':
+    ensure => latest,
+  }
+  -> file_line { 'fish_shell':
+    path => '/etc/shells',
+    line => '/usr/local/bin/fish',
+  }
+
+  # Can't add a relationship here since the homebrew install has an implicit dependency on the user
   user { $::boxen_user:
     shell => '/usr/local/bin/fish',
-    require => Package['fish'],
+  }
+
+  # override global gitignore
+  Git::Config::Global <| title == "core.excludesfile" |> {
+    value => "~/.gitignore_global"
   }
 }
